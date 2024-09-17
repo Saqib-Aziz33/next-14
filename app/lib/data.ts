b4e51385a140f4579a7472ee0d6c7c774c580017
zimo-frontend-dev-saqib <saqib.aziz@zimogroup.org>
2024-09-17 22:50:04 +0500

    chapter 9 finsihed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,15 @@ import prisma from "./db";
 
 export async function fetchRevenue() {
   try {
+    // We artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await prisma.revenue.findMany();
+
+    console.log("Data fetch completed after 3 seconds.");
+
     return data;
   } catch (error) {
     console.error("Database Error:", error);
